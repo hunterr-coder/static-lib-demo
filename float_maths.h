d6821f65e4fb8ebfec4f2ef9743a46cca48bf7c6
sams-nip <sonu.pura.2017@gmail.com>
2018-02-25 20:52:04 +0530

    Initial commit

---
+++ float_maths.h
@@ -0,0 +1,7 @@
+#ifndef FLOAT_MATHS_H
+#define FLOAT_MATHS_H
+
+float add(float , float);
+float sub(float, float);
+
+#endif
---
+++ int_maths.h
@@ -0,0 +1,7 @@
+#ifndef INT_MATHS_H
+#define INT_MATHS_H
+
+int add(int , int );
+int sub(int , int );
+
+#endif
